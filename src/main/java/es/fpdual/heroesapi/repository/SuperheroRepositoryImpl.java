0fccad1e81ee03373d4d0d1085c82f11004c9b32
Deg42 <jgonzalez@eci.safanet.es>
2022-02-14 12:21:09 +0100

    Transactions now in service

--- src/main/java/es/fpdual/heroesapi/repository/SuperheroRepositoryImpl.java
+++ src/main/java/es/fpdual/heroesapi/repository/SuperheroRepositoryImpl.java
@@ -5,10 +5,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.TransactionDefinition;
-import org.springframework.transaction.TransactionStatus;
-import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 import es.fpdual.heroesapi.model.SuperheroBean;
 
@@ -17,9 +13,6 @@ public class SuperheroRepositoryImpl implements SuperheroRepository {
 
 	@Autowired
 	private JdbcTemplate template;
-	
-	@Autowired
-	private PlatformTransactionManager transactionManager;
 
 	@Override
 	public List<SuperheroBean> selectAll() {
@@ -43,60 +36,38 @@ public class SuperheroRepositoryImpl implements SuperheroRepository {
 
 	@Override
 	public void insert(SuperheroBean superhero) {
-				
-			// 1. Define transaction
-			TransactionDefinition def = new DefaultTransactionDefinition();
-			TransactionStatus status = transactionManager.getTransaction(def);
-			
-			// 2. Open transaction
 			try {
 			this.template.update("INSERT INTO HEROES VALUES (?, ?, ?, ?)", 
 					superhero.getId(), 
 					superhero.getName(),
 					superhero.getAlterego(),
 					superhero.getImage());
-		
-			// 3. Commit or Rollback
-			transactionManager.commit(status);
 			
 		} catch (Exception e) {
-			transactionManager.rollback(status);
 			e.printStackTrace();
 			throw e;
 		}
 	}
 
 	@Override
-	public void update(SuperheroBean superhero) {
-		TransactionDefinition def = new DefaultTransactionDefinition();
-		TransactionStatus status = transactionManager.getTransaction(def);
-		
+	public void update(SuperheroBean superhero) {		
 		try {
 			this.template.update("UPDATE HEROES SET NAME = ?, ALTEREGO = ?, IMG = ? WHERE ID = ?",
 					superhero.getName(),
 					superhero.getAlterego(),
 					superhero.getImage(),
 					superhero.getId());
-			
-			transactionManager.commit(status);
 		} catch (Exception e) {
-			transactionManager.rollback(status);
 			e.printStackTrace();
 			throw e;
 		}
 	}
 
 	@Override
-	public void delete(long id) {
-		TransactionDefinition def = new DefaultTransactionDefinition();
-		TransactionStatus status = transactionManager.getTransaction(def);
-		
+	public void delete(long id) {		
 		try {
 			this.template.update("DELETE FROM HEROES WHERE ID = ?", id);
-			
-			transactionManager.commit(status);
 		} catch (Exception e) {
-			transactionManager.rollback(status);
 			e.printStackTrace();
 			throw e;
 		}
--- src/main/java/es/fpdual/heroesapi/service/SuperheroServiceImpl.java
+++ src/main/java/es/fpdual/heroesapi/service/SuperheroServiceImpl.java
@@ -4,11 +4,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import es.fpdual.heroesapi.model.SuperheroBean;
 import es.fpdual.heroesapi.repository.SuperheroRepository;
 
 @Service
+@Transactional
 public class SuperheroServiceImpl implements SuperheroService {
 
 	@Autowired
