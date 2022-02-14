8a08624f1ac967c0e8964ff1afb48af5125cec99
Deg42 <jgonzalez@eci.safanet.es>
2022-02-14 11:31:24 +0100

    Transactional queries

--- src/main/java/es/fpdual/heroesapi/repository/SuperheroRepositoryImpl.java
+++ src/main/java/es/fpdual/heroesapi/repository/SuperheroRepositoryImpl.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 import es.fpdual.heroesapi.model.SuperheroBean;
 
@@ -14,7 +17,7 @@ public class SuperheroRepositoryImpl implements SuperheroRepository {
 
 	@Autowired
 	private JdbcTemplate template;
-
+	
 	@Autowired
 	private PlatformTransactionManager transactionManager;
 
@@ -40,10 +43,24 @@ public class SuperheroRepositoryImpl implements SuperheroRepository {
 
 	@Override
 	public void insert(SuperheroBean superhero) {
-		try {
-			this.template.update("INSERT INTO HEROES VALUES (?, ?, ?, ?)", superhero.getId(), superhero.getName(),
-					superhero.getAlterego(), superhero.getImage());
+				
+			// 1. Define transaction
+			TransactionDefinition def = new DefaultTransactionDefinition();
+			TransactionStatus status = transactionManager.getTransaction(def);
+			
+			// 2. Open transaction
+			try {
+			this.template.update("INSERT INTO HEROES VALUES (?, ?, ?, ?)", 
+					superhero.getId(), 
+					superhero.getName(),
+					superhero.getAlterego(),
+					superhero.getImage());
+		
+			// 3. Commit or Rollback
+			transactionManager.commit(status);
+			
 		} catch (Exception e) {
+			transactionManager.rollback(status);
 			e.printStackTrace();
 			throw e;
 		}
@@ -51,10 +68,19 @@ public class SuperheroRepositoryImpl implements SuperheroRepository {
 
 	@Override
 	public void update(SuperheroBean superhero) {
+		TransactionDefinition def = new DefaultTransactionDefinition();
+		TransactionStatus status = transactionManager.getTransaction(def);
+		
 		try {
-			this.template.update("UPDATE HEROES SET NAME = ?, ALTEREGO = ?, IMG = ? WHERE ID = ?", superhero.getName(),
-					superhero.getAlterego(), superhero.getImage(), superhero.getId());
+			this.template.update("UPDATE HEROES SET NAME = ?, ALTEREGO = ?, IMG = ? WHERE ID = ?",
+					superhero.getName(),
+					superhero.getAlterego(),
+					superhero.getImage(),
+					superhero.getId());
+			
+			transactionManager.commit(status);
 		} catch (Exception e) {
+			transactionManager.rollback(status);
 			e.printStackTrace();
 			throw e;
 		}
@@ -62,9 +88,15 @@ public class SuperheroRepositoryImpl implements SuperheroRepository {
 
 	@Override
 	public void delete(long id) {
+		TransactionDefinition def = new DefaultTransactionDefinition();
+		TransactionStatus status = transactionManager.getTransaction(def);
+		
 		try {
 			this.template.update("DELETE FROM HEROES WHERE ID = ?", id);
+			
+			transactionManager.commit(status);
 		} catch (Exception e) {
+			transactionManager.rollback(status);
 			e.printStackTrace();
 			throw e;
 		}
